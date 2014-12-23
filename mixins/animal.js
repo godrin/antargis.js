d0acb20cf39aa3ab4b8e1361d32b67c3939fd7c1
David Kamphausen <david.kamphausen@web.de>
2014-12-23 17:03:18 +0100

    fix animal

--- mixins/animal.js
+++ mixins/animal.js
@@ -1,6 +1,7 @@
 define(["mixins/lljobs/move","mixins/lljobs/rest"],function(LlMoveJob,LlRestJob) {
   return {
-    onNoLlJob:function(delta) {
+    onNoJob:function(delta) {
+
       if(Math.random()<0.5) {
         this.setMesh("walk");
         var targetPos=new THREE.Vector2(Math.random()*2-1,Math.random()*2-1).add(this.pos);
