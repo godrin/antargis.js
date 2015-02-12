487afec89e4be3400b1df7d4c5e75d1bdca1d117
David Kamphausen <david.kamphausen@web.de>
2015-02-12 19:58:23 +0100

    add anuglar

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var mainBowerFiles = require('main-bower-files');
 var libs=['bower_components/threejs-examples/examples/js/loaders/OBJLoader.js'].concat(mainBowerFiles());
 
 gulp.task('debug',function() {
-  return gulp.src(libs).pipe(gulp.dest('public/js/libs'));
+  return gulp.src(libs).pipe(gulp.dest('js/libs'));
 });
 
 gulp.task('default',['debug']);
