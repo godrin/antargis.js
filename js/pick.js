d912b47f4668598dcad25879a9b83398239c36a1
David Kamphausen <david.kamphausen@web.de>
2015-03-11 19:52:43 +0100

    cleanup

--- js/pick.js
+++ js/pick.js
@@ -13,21 +13,11 @@ define([],function() {
     // create a Ray with origin at the mouse position
     //   and direction into the scene (camera direction)
     var vector = new THREE.Vector3( mouse.rx, mouse.ry, 0 );
-    /*
-    projector.unprojectVector( vector, camera );
-    console.log("VVV2",vector);
-    var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );*/
-
-    //var ray= projector.pickingRay( vector.clone(), camera );
-
-var myvector = vector.clone().unproject( camera );
-var direction = new THREE.Vector3( 0, 0, -1 ).transformDirection( camera.matrixWorld );
-raycaster.set( myvector, direction );
-//var intersects = raycaster.intersectObjects( objects );
-
+    var myvector = vector.clone().unproject( camera );
+    var direction = new THREE.Vector3( 0, 0, -1 ).transformDirection( camera.matrixWorld );
+    raycaster.set( myvector, direction );
 
     // create an array containing all objects in the scene with which the ray intersects
-    
     // intersect recursive !!! 
     return raycaster.intersectObjects( scene.children,true);
   };
