2fd8009a73253cbf8cc221d24b2495b4db15aa89
David Kamphausen <david.kamphausen@web.de>
2015-11-19 20:25:24 +0100

    something to see

--- js/ng_game.js
+++ js/ng_game.js
@@ -1,41 +1,100 @@
-define(["app2", "base"], function(app,_three, Base) {
+define(["app2", "base", "generator", "heightmap", "level", "world", "skybox", "terrain"], function(app, Base, Generator, HeightMap, Level, World, Skybox, Terrain) {
 
   app.controller('GameController', function($scope) {});
 
+  app.factory('World', function($q) {
+    return {
+      createWorld: function(w, levelName) {
+        return $q(function(resolve, reject) {
+          var mapOptions = {
+            width: w,
+            height: w
+          };
 
-  function makeSceneAndCamera($element) {
-    var data = {};
-    data.scene = new THREE.Scene();
-    var w = $element.width();
-    var h = $element.height();
-    var camera = data.camera = new THREE.PerspectiveCamera(60, w / h, 1, 10000);
+          Generator(mapOptions, function(w, h, data) {
+            data = HeightMap.pickGreen(w, h, data)
 
-    data.renderer = new THREE.WebGLRenderer();
+            var map = new HeightMap({
+              width: w,
+              height: w,
+              map: {
+                rock: data
+              }
+            });
+            var world = new World(map);
+            Level.load(levelName, map, world, function() {
+              resolve(world);
+            });
+          });
+        });
+      }
+    };
+  });
 
-    data.renderer.setSize(w, h);
-    $element.prepend(data.renderer.domElement);
-    $(data.renderer.domElement).addClass("game-view");
 
-    camera.position.x = 16;
-    camera.position.y = -5;
-    camera.position.z = 10;
-    camera.rotation.x = (10 + 32) * Math.PI / 180;
-
-    data.camera.aspect = w / h;
-    data.camera.updateProjectionMatrix();
-
-    data.renderer.setSize(w, h);
-    if (false) {
-      data.particleGroup = this.makeSPEGroup();
-      data.particleGroup.addPool(10, this.emitterSettings, false);
-      data.scene.add(data.particleGroup.mesh);
-      data.scene.particleGroup = data.particleGroup;
-    }
-    return data;
-  }
+  // Store world in rootScope
+  app.run(function($rootScope, World) {
+    World.createWorld(64, "tests/fetch.js").then(function(world) {
+      $rootScope.world = world;
+    });
+  });
 
   app.controller('GameView', function($scope, $element) {
-    var data = new Base($element);//makeSceneAndCamera($element);
+    var data = new Base($element);
+
+    $scope.$watch("world", function() {
+      console.log("WWWWORLD", $scope.world);
+      if ($scope.world)
+        initScene($scope.world);
+    });
+
+    function initScene(world) {
+      var base = data;
+      var geometry = new THREE.BoxGeometry(1, 1, 1);
+      var scene = base.scene;
+
+      // soft white light
+      var light = new THREE.AmbientLight(0x202020);
+      scene.add(light);
+
+      // White directional light at half intensity shining from the top.
+      var directionalLight = new THREE.DirectionalLight(0xffffff, 0.7);
+      directionalLight.position.set(1, 0, 0.7);
+      scene.add(directionalLight);
+
+      Skybox.add(scene);
+
+      var map = world.map;
+
+      var threeHeightMap = map.toThreeTerrain();
+
+      Terrain.create(map, scene, threeHeightMap);
+
+      // FIXME: load all models beforehand
+      world.initScene(scene);
+
+      render();
+    }
+    var lastPos = null;
+
+    function render() {
+      var base = data;
+      var world = $scope.world;
+      base.render({
+        frameCallback: function(delta) {
+          if (!world.pause) {
+            _.each(world.entities, function(e) {
+              if (e && e.onFrame)
+                e.onFrame(delta);
+            });
+            if (world && world.$scope)
+              world.$scope.$apply();
+            else
+              console.log("NO APPLY");
+          }
+        }
+      });
+    }
     $scope.scene = data.scene;
   });
 
