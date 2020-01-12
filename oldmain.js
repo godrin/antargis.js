00622ecf2357d789645e1093cad693da48944b3d
David Kamphausen <david.kamphausen@innoq.com>
2020-01-12 18:30:47 +0100

    cleanup

---
+++ oldmain.js
@@ -0,0 +1,6 @@
+var angular =  require('angular');
+var app = angular.module("text",[]);
+
+app.controller('AppController', function($scope) {
+  $scope.test="TEST";
+});
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './intro.js';
 import './credits.js';
+import './campaign.js';
 import '../menu.scss';
